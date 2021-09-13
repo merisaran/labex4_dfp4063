98a7bf321629752bb9eec2a7535b6c105f806d36
merisaran <merisanik00@gmail.com>
2021-09-13 15:55:57 +0800

    pertama kali push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merisa Labex4</title>
+</head>
+<body>
+    <center>
+        <h1>Terminology</h1>
+        <table cellpadding="40" border="1">
+            <th><b>Terms</b></th>
+            <th><b>Description</b></th>
+            </tr>
+            <tr>
+                <td><br>[<a href="">Add New</a>]<br></td>
+                <td>
+                    <p></p>
+                </td>
+            </tr>
+            </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
